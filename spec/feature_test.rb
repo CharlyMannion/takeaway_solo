5b1d7cd0eed685d599ce557580c3c8176d9bd0df
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-18 11:19:54 +0000

    checks menu in feature tests

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,9 +1,17 @@
-require 'order'
-require 'menu'
-
 # to run in irb:
-# require './lib/order.rb'
-# require './lib/menu.rb'
+require './lib/order.rb'
+require './lib/menu.rb'
 
 # to run programme:
 # ruby ./lib/order.rb
+# ruby ./lib/menu.rb
+
+# to run feature tests:
+# ruby './spec/feature_test.rb'
+
+# menu feature test:
+menu = Menu.new
+menu.add(:tuna, 10)
+menu.add(:cod, 12)
+menu.add(:chips, 5)
+p menu.items
